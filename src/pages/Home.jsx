a30749559b41088ee5fadd3b575f121f97601796
ahlam201601 <elhadriahlam9@gmail.com>
2025-12-23 15:13:53 +0100

    initialisation projet

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div class="flex items-center justify-center h-screen bg-gray-100 text-3xl font-bold text-yellow-600">Home</div>
+  )
+}
