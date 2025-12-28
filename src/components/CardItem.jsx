a313d2ec3faa8de6fd6628a5fbf7f3d384c03733
ahlam201601 <elhadriahlam9@gmail.com>
2025-12-28 22:36:36 +0100

    Added incrementing and decrementing of the quantity

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,4 +1,12 @@
+import { useDispatch } from "react-redux";
+import {
+  decreaseQuantity,
+  increaseQuantity
+} from "../lib/cartSlice";
+import { FaTrash } from "react-icons/fa";
+
 export default function CardItem({ item }) {
+  const dispatch = useDispatch();
   
 
   return (
@@ -12,7 +20,34 @@ export default function CardItem({ item }) {
       <div className="flex-1">
         <p className="font-semibold">{item.name}</p>
         <p className="text-sm text-gray-200">{item.price} $</p>
+
+        {/* Quantity controls */}
+        <div className="flex items-center gap-2 mt-2">
+          <button
+            onClick={() => dispatch(decreaseQuantity(item.id))}
+            className="w-6 h-6 bg-gray-300 rounded font-bold"
+          >
+            -
+          </button>
+
+          <span className="font-semibold">{item.quantity}</span>
+
+          <button
+            onClick={() => dispatch(increaseQuantity(item.id))}
+            className="w-6 h-6 bg-gray-300 rounded font-bold"
+          >
+            +
+          </button>
+        </div>
       </div>
+
+      {/* Remove item */}
+      {/* <button
+        onClick={() => dispatch(removeCart(item.id))}
+        className="text-red-500 font-bold cursor-pointer"
+      >
+        <FaTrash />
+      </button> */}
     </div>
   );
 }
--- src/lib/cartSlice.js
+++ src/lib/cartSlice.js
@@ -21,8 +21,23 @@ const cartSlice = createSlice({
       }
       state.totalQuantity++;
     },
+    increaseQuantity: (state, action) => {
+      const item = state.items.find((i) => i.id === action.payload);
+      if (item) {
+        item.quantity += 1;
+        state.totalQuantity += 1;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const item = state.items.find((i) => i.id === action.payload);
+      if (item && item.quantity > 1) {
+        // ✅ seulement si quantity > 1
+        item.quantity -= 1;
+        state.totalQuantity -= 1;
+      }
+    },
   },
 });
 
-export const { addToCart} = cartSlice.actions;
+export const { addToCart , increaseQuantity , decreaseQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
